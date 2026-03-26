0c075802ee521e06f087e729f6142c82e7d4f509
boyninja1555 <flappyfloorg@gmail.com>
2026-03-26 00:41:08 -0500

    Floorquery can now determine param formats and return types from the operator!!!

--- floorquery/floorquery.d.ts
+++ floorquery/floorquery.d.ts
@@ -1,6 +1,18 @@
 export { }
 declare global {
     /**
+     * Maps operators to param type helpers.
+     */
+    type QueryMap = {
+        el: { params: string; return: Element | null }
+        EL: { params: string; return: NodeListOf<Element> }
+        ls: { params: string; return: string | null }
+        LS: { params: `${string};${string}`; return: void }
+        ss: { params: string; return: string | null }
+        SS: { params: `${string};${string}`; return: void }
+    }
+
+    /**
      * ### INTERNAL HELPER
      * 
      * Returns Error, used when a query-related problem happens. Fill in operator and params correctly for best debugging experience.
@@ -15,5 +27,5 @@ declare global {
      * @param operator Typed operator which uses params
      * @param params Extra info optionally used by operator
      */
-    function $(operator: "el" | "EL" | "ls" | "LS" | "ss" | "SS", params: string): any
+    function $<K extends keyof QueryMap>(operator: K, params: QueryMap[K]["params"]): QueryMap[K]["return"]
 }
